aa0492432b0752a4b30dca4cc0554fb0c95dba26
joshbuzztech <josh@buzztech.ng>
2024-08-03 21:49:44 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,40 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import React from 'react'
+
+const Home = () => {
+    const loggedIn = {firstName : 'BigJosh', email:'adelobajoshua19@gmail.com'}
+  return (
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+               <HeaderBox 
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account and transaction effeciently"
+               />
+
+               <TotalBalanceBox 
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1258.35}
+               />
+
+            </header>
+
+            RECENT TRANSACTIONS
+        </div>
+
+        <RightSidebar 
+          user={loggedIn}
+          transactions={[]}
+          banks={[{currentBalance: 123.5}, {currentBalance: 500.5}]}
+        />
+
+    </section>
+  )
+}
+
+export default Home
