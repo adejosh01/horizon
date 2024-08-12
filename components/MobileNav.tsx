37d7eeaf16cd481bd41aef727aeedc9c1bb9451c
joshbuzztech <josh@buzztech.ng>
2024-08-12 23:58:15 +0100

    finish touches

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -15,6 +15,7 @@ import { sidebarLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import Footer from './Footer'
+import PlaidLink from './PlaidLink'
 
 const MobileNav = ({ user }: MobileNavProps) => {
     const pathname = usePathname()
@@ -72,7 +73,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                                 
                             )
                         })}
-                        USER
+                      <PlaidLink user={user} />
                                 </nav>
                             </SheetClose>
 
